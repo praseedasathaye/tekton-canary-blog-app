6e5c250697f8c5012f73e626e94b7029c2c467f8
Valentin Widmer <widmerva@amazon.com>
2022-06-07 17:42:16 +0200

    Release version 5 of my app

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var AWSXRay = require('aws-xray-sdk');
 app.use(AWSXRay.express.openSegment('Product-Detail-V1'));
 
 app.get("/catalogDetail", (req, res, next) => {
-  console.log("Catalog Detail Version 4 Get Request Successful");
+  console.log("Catalog Detail Version 5 Get Request Successful");
   res.json({
-             "version":"4",
+             "version":"5",
              "vendors":[ "ABC.com", "XYZ.com", "PQR.com", "SPAIN.com"]
               } )
 });
