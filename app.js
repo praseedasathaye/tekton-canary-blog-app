710e7efae3a8da269b6bbf4e5e6b3cb416e5ea28
Valentin Widmer <widmerva@amazon.com>
2022-07-03 21:02:56 +0200

    Release v2.0.0

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var AWSXRay = require('aws-xray-sdk');
 app.use(AWSXRay.express.openSegment('Product-Detail-V1'));
 
 app.get("/catalogDetail", (req, res, next) => {
-  console.log("Catalog Detail Version 1 Get Request Successful");
+  console.log("Catalog Detail Version 2 Get Request Successful");
   res.json({
-             "version":"1",
-             "vendors":[ "ABC.com", "XYZ.com", "PQR.com", "SPAIN.com"]
+             "version":"2",
+             "vendors":[ "ABC.com", "XYZ.com", "PQR.com", "SPAIN.com", "SWITZERLAND.com"]
               } )
 });
 
@@ -19,7 +19,7 @@ app.get("/ping", (req, res, next) => {
   res.json("Healthy")
 });
 
-app.use(AWSXRay.express.closeSegment());
+app.use(AWSXRay.express.closeSegment()); 
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
