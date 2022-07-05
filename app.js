dd8e784584909974f86b239483766943b5d85807
Valentin Widmer <widmerva@amazon.com>
2022-07-05 23:02:06 +0200

    Reset Demo

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ var AWSXRay = require('aws-xray-sdk');
 app.use(AWSXRay.express.openSegment('Product-Detail-V1'));
 
 app.get("/catalogDetail", (req, res, next) => {
-  console.log("Catalog Detail Version 2 Get Request Successful");
+  console.log("Catalog Detail Version 1 Get Request Successful");
   res.json({
-             "version":"2",
-             "vendors":[ "ABC.com", "XYZ.com", "PQR.com", "SPAIN.com", "SWITZERLAND.com"]
+             "version":"1",
+             "vendors":[ "ABC.com", "XYZ.com", "PQR.com", "SPAIN.com"]
               } )
 });
 
